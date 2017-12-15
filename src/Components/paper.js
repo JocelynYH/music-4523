3ba0b719e8087fe7df1f29b2d0fd54331094c181
JocelynYH <seaweed4@live.com>
2017-12-15 17:39:31 -0500

    need to push before pull

--- src/Components/paper.js
+++ src/Components/paper.js
@@ -11,8 +11,7 @@ class Paper extends Component {
                     </div>
                
                     <h1>"Gqom and the Future of Genre in South Africa" </h1>
-                    <p>The futuristic dance style of gqom, with its broken beats, dark drones, and growling samples, has had an interesting evolution since its rise in the clubs of Durban. Gqom became popular in Europe long before it was widely popular in its native South Africa, partially due to its  Through the "gqomwave" music of Okmalumkoolkat, aka Smiso Zwane, gqom has demonstrated its portability as a genre, woven into American trap and hip-hop aesthetics. Okmalumkoolkat's collaboration with Rudeboyz shows Zwane's willingness to experiment with all of the styles and genres surrounding him. 
-</p>
+                    <p>The futuristic dance style of gqom, with its broken beats, dark drones, and growling samples, has had an interesting evolution since its rise in the clubs of Durban. Gqom became popular in Europe long before it was widely popular in its native South Africa, partially due to its  Through the "gqomwave" music of Okmalumkoolkat, aka Smiso Zwane, gqom has demonstrated its portability as a genre, woven into American trap and hip-hop aesthetics. Okmalumkoolkat's collaboration with Rudeboyz shows Zwane's willingness to experiment with all of the styles and genres surrounding him. </p>
                 
                 </section>
 
