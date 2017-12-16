90104e101618ad775836b9c9a7508899c79535f8
linshavers <linshavers@gmail.com>
2017-12-16 08:06:45 -0500

    Add coming soon tag to interview page

--- src/Components/interview.js
+++ src/Components/interview.js
@@ -12,13 +12,14 @@ class Interview extends Component {
                     </div>
                         <h1>A Hard Fela to Follow: The Origin Story of Afrobeat</h1>
                         <p>In the video, A Hard Fela to Follow: The Origin Story of Afrobeat, we interview some students to discuss the narrative of Fela Kuti’s Afrobeat. Through this process, interviewees are asked a series of questions that deal with Afrobeat and Afrobeats. With this engaging dialogue, interviewees analyze Fela's work and songs that have sampled his work. Ultimately, the interviewees are asked whether these Afrobeats' songs are paying homage to Fela or just a strategic ploy to get more viewers?</p>
-                                          
+
                  </div>
                 </section>
 
                 <section className='content interview black'>
                     <div class="container">
-                        <p>Content: 1. [Insert video as an embedded youtube video.  2. a timeline</p>
+                    {/*<p>Content: 1. [Insert video as an embedded youtube video.  2. a timeline</p>*/}
+                          <h1>Coming Soon!</h1>
                     </div>
                 </section>
 
